06ee1b7f5fda641a447ede721f0174698f1ded62
pete <blink182>
2017-03-13 12:57:29 +0000

    Password reset styling and updated ReadMe

--- readme.md
+++ readme.md
@@ -7,3 +7,15 @@ This project is based on Laravel, you will need to init the following methods, o
 - php artisan migrate
 
 This framework features a standard Laravel auth scaffold and a paid for admin theme that has replaced the standard Laravel styling
+
+
+##To Do
+
+- Style password reset email
+- Set email variables
+- Add admin panel styling to
+  - Homepage
+  - Sidebar
+  - Header
+    - Logged Out
+    - Logged In
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,70 +2,27 @@
 
 @section('content')
     <div class="login-form">
-
         <div class="login-content">
-
-            <form method="post" role="form" id="form_forgot_password" novalidate="novalidate">
-
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+                {{ csrf_field() }}
                 <div class="form-forgotpassword-success">
                     <i class="entypo-check"></i>
                     <h3>Reset email has been sent.</h3>
                     <p>Please check your email, reset password link will expire in 24 hours.</p>
                 </div>
-
                 <div class="form-steps">
-
                     <div class="step current" id="step-1">
-
-                        <div class="form-group">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <div class="input-group">
                                 <div class="input-group-addon">
                                     <i class="entypo-mail"></i>
                                 </div>
-
-                                <input type="text" class="form-control" name="email" id="email" placeholder="Email" data-mask="email" autocomplete="off">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-info btn-block btn-login">
-                                Complete Registration
-                                <i class="entypo-right-open-mini"></i>
-                            </button>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </form>
-
-
-
-        </div>
-
-    </div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
+                                <input type="text" class="form-control" name="email" id="email" placeholder="Email" data-mask="email" autocomplete="off" value="{{ old('email') }}" required>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -73,18 +30,15 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
+                            <button type="submit" class="btn btn-info btn-block btn-login">
+                                Send Password Reset Link
+                                <i class="entypo-right-open-mini"></i>
+                            </button>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
